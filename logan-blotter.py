6059e670e303627d5d281dc2483e10d954b74aed
Mike Gallagher <gallaghermikey@gmail.com>
2015-08-23 19:28:19 -0600

    Cleaning up before making public

--- logan-blotter.py
+++ logan-blotter.py
@@ -1,12 +1,10 @@
 import json
 import urllib2
 import os
-import fileinput
 import calendar
 import re
 from peewee import *
 from datetime import datetime
-
 
 db_path = os.path.abspath('./logan_blotter.db')
 db = SqliteDatabase(db_path)
@@ -23,26 +21,28 @@ class Report(Model):
         order_by = ('date_reported',)
         database = db
 
+def delete_all_records():
+    for r in Report.select():
+        r.delete_instance()
+
 db.connect()
 
 data = urllib2.urlopen('http://www.loganutah.org/Police/Reports/json/calls.cfm?hours=12')
 reports = json.load(data)['DATA']
-# reports = [[str(r).rstrip('\n\n') for r in report] for report in reports]
 
 day_abrv = [ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" ]
 today = datetime.today()
 month_abbr = calendar.month_abbr[today.month]
 
-
 reports = [[r for r in report] for report in reports]
 
-print "Before", Report.select().count()
-# import pdb; pdb.set_trace()
+count_before = Report.select().count()
 
 for r in reports:
 
-    # Narrative
-    # Find a year in the string and strip away all of it
+    # The narrative field requires a lot of clean-up with regex
+    # First we find a year in the string and strip away all of it
+    # We also remove trailing whitespace and extra carriage returns
     strip_year = r"^.*\b" + str(today.year) + "(.*?\n\n|.*?\s\s|\s\b|.*?Powell\s)"
     strip_line_breaks = r"(\n|\n\n)"
     strip_trailing_spaces = r"(^\s*|\s*$)"
@@ -50,81 +50,18 @@ for r in reports:
     narrative = re.sub(strip_line_breaks, " ", narrative)
     narrative = re.sub(strip_trailing_spaces, "", narrative)
 
-    # print narrative
-    # print r[0].rstrip()
-    # r = Report.create(
-    # incident_num  = r[0].rstrip(),
-    # date_reported = datetime.strptime(r[1], "%B, %d %Y %H:%M:%S"),
-    # dispos        = r[2].rstrip(),
-    # nature        = r[3].rstrip(),
-    # agency        = r[4],
-    # narrative     = narrative
-    # )
-
-    # new_report = Report()
-    # new_report.incident_num  = r[0].rstrip(),
-    # new_report.date_reported = datetime.strptime(r[1], "%B, %d %Y %H:%M:%S"),
-    # new_report.dispos        = r[2].rstrip(),
-    # new_report.nature        = r[3].rstrip(),
-    # new_report.agency        = r[4],
-    # new_report.narrative     = narrative
-
-    # import pdb; pdb.set_trace()
+    # get_or_create() allows us to insert only unique records
     entry, created = Report.get_or_create(
         incident_num=r[0].rstrip(),
-
         defaults={
             'date_reported': datetime.strptime(r[1], "%B, %d %Y %H:%M:%S"),
             'dispos' : r[2].rstrip(),
             'nature' : r[3].rstrip(),
             'agency' : r[4],
-            'narrative' : narrative } )
-    # year_pos = r[5].find(str(today.year), 15, 35)
-    # if year_pos > 0:
+            'narrative' : narrative } 
+        )
 
-    #     date_rmvd = r[5][year_pos+4:]
-    #     #     import pdb; pdb.set_trace()
-    #     line_breaks = date_rmvd.find("\n\n", 0, 25)
-    #     # print date_rmvd
-    #     date_rmvd = date_rmvd.replace("\n", " ")
-    #     if line_breaks >= 0:
-    #         # print "line_breaks FOUND"
-    #         date_rmvd = date_rmvd[line_breaks+2:]
-    #         print date_rmvd
-    #     else:
-    #         date_rmvd = date_rmvd.replace("  ", "", 1)
-    #         # print date_rmvd
+print "Update successful"
+print Report.select().count() - count_before, "records inserted"
+delete_records()
 
-    # else:
-    #     pass
-        # print r[5]
-    # if r[5].split(' ', 1)[0] in day_abrv:
-    #     print r[5].split(' ', 1)[0]
-    # else:
-    #     print "nope"
-    # import pdb; pdb.set_trace()
-    # break
-print "After", Report.select().count()
-# print j['COLUMNS']
-# print "records returned:", len(j['DATA'])
-# August, 21 2015 20:24:41
-# print j['DATA'][0][1]
-# cols = [u'NUMBER', u'DTREPOR', u'DISPOS', u'NATURE', u'AGENCY', u'NARRATV']
-# date = datetime.strptime(j['DATA'][0][1], "%B, %d %Y %H:%M:%S")
-
-# for report in reports['DATA']:
-
-#     r = Report.create(
-#         incident_num  = report[0],
-#         date_reported = datetime.strptime(report[1], "%B, %d %Y %H:%M:%S"),
-#         dispos        = report[2].rstrip(),
-#         nature        = report[3].rstrip(),
-#         agency        = report[4],
-#         narrative     = report[5]
-#         )
-
-# rep = Report.create(incident_num="abcd", date_reported=datetime.today(), 
-#     dispos="blah", nature="blahaa", agency="YES", narrative="STUFF HAPPENED")
-# import pdb; pdb.set_trace()
-# for r in Report.select():
-#     r.delete_instance()
